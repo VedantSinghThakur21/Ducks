6ae84e2ca407d7ce18db828f41d7ea5d2ebe0d33
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2025-03-08 22:32:37 +0530

    Setting QuackBehaviour of Duck

--- src/Duck.java
+++ src/Duck.java
@@ -2,4 +2,9 @@ abstract public class Duck {
     QuackBehaviour quackBehaviour;
     SwimBehaviour swimBehaviour;
     FlyBehaviour flyBehaviour;
+
+    public void setQuackBehaviour(QuackBehaviour quackBehaviour) {
+        this.quackBehaviour = quackBehaviour;
+    }
+
 }
