536999cb68576a17beb72bad9fe98e63e477dcce
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2025-03-08 22:37:38 +0530

    Adding Implementation and Defining Behaviour of DecoyDuck

--- src/DecoyDuck.java
+++ src/DecoyDuck.java
@@ -1,5 +1,12 @@
 public class DecoyDuck extends Duck {
     public DecoyDuck(){
+        quackBehaviour = new Quack();
+        swimBehaviour = new Floating();
+        flyBehaviour = new NotFly();
 
+
+    }
+    public void display() {
+        System.out.println("I am Decoy Duck");
     }
 }
