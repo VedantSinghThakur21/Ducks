ea68b281eca50d8d07da65a8549d9e243b5b61b1
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2025-03-08 22:38:05 +0530

    Adding Implementation and Defining Behaviour of RedHeadDuck

--- src/RedHeadDuck.java
+++ src/RedHeadDuck.java
@@ -1,5 +1,12 @@
 public class RedHeadDuck extends Duck {
     public RedHeadDuck() {
+        quackBehaviour = new Quack();
+        swimBehaviour = new Swimming();
+        flyBehaviour = new Flying();
+    }
 
+    @Override
+    public void display() {
+        System.out.println("I'm a red head duck!!");
     }
 }
