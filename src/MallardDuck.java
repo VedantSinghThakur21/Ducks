88287dc42010690e69a7709a8c1be457d3c2f65b
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2025-03-08 22:37:19 +0530

    Adding Implementation and Defining Behaviour of MallardDuck

--- src/MallardDuck.java
+++ src/MallardDuck.java
@@ -3,6 +3,13 @@ import java.io.Flushable;
 public class MallardDuck extends Duck {
 
     public MallardDuck(){
+        quackBehaviour = new Quack();
+        swimBehaviour = new Swimming();
+        flyBehaviour = new Flying();
 
+
+    }
+    public void display() {
+        System.out.println("I am Mallard Duck");
     }
 }
