5737d8b053326365535f2581b842cb5c1415ce6d
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2025-03-10 09:53:24 +0530

    Edits made in Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//Name - Vedant Singh Thakur
+// PRN - 23070126144
+// batch - AIML B3
 public class Main {
     public static void main(String[] args) {
         MallardDuck md = new MallardDuck();
